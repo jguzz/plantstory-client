bb7463033e6a62c1ebd60793bc5ba1a949965823
jguzz <joseph.a.guzzardo@gmail.com>
2020-06-08 09:51:42 -0400

    basic routes set up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import {Route,Switch} from 'react-router-dom'
+// Components
+import Login from './components/auth/Login'
+import Signup from './components/auth/Signup'
+import NewPost from './components/create/NewPost'
+import NewStory from './components/create/NewStory'
+import MainFeed from './components/home/MainFeed'
+import Navbar from './components/nav/Navbar'
+import Profile from './components/profile/Profile'
+
 
 function App() {
   return (
-    <>App</>
+    <>
+    <Navbar/>
+    <Switch>
+      <Route path="/login" render={() => <Login/>} />
+      <Route path="/signup" render={() => <Signup/>} />
+      <Route path="/newPost" render={() => <NewPost/>} />
+      <Route path="/newStory" render={() => <NewStory/>} />
+      <Route path="/mainfeed" render={() => <MainFeed/>}/>
+      <Route path="/profile" render={() => <Profile/>}/>
+    </Switch>
+    </>
   );
 }
 
