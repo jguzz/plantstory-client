fcb94098aaa96cd9a89ae717957b8cbb232e764d
jguzz <joseph.a.guzzardo@gmail.com>
2020-06-08 13:25:09 -0400

    data is being fetched from the backend

--- src/App.js
+++ src/App.js
@@ -10,8 +10,36 @@ import Navbar from "./components/nav/Navbar";
 import BottomNav from "./components/nav/BottomNav";
 import Profile from "./components/profile/Profile";
 
+const BASE_URL = 'http://localhost:3000'
+const USER_URL = `${BASE_URL}/users`
+const PLANT_URL = `${BASE_URL}/plants`
+const LIKE_URL = `${BASE_URL}/likes`
+const POST_URL = `${BASE_URL}/posts`
+const STORY_URL = `${BASE_URL}/stories`
+
 class App extends React.Component {
+  state = {
+    users: [],
+    plants: []
+  }
+  //Fetching
+  componentDidMount() {
+    this.fetchAll()
+  }
+  fetch = (url, name) => {
+    fetch(url)
+    .then(res => res.json())
+    .then(data => this.setState({[name]: data}))
+  }
+  fetchAll = () => {
+    this.fetch(USER_URL, 'users')
+    this.fetch(PLANT_URL, 'plants')
+    this.fetch(LIKE_URL, 'likes')
+    this.fetch(POST_URL, 'posts')
+    this.fetch(STORY_URL, 'stories')
+  }
   render() {
+    console.log(this.state)
     return (
       <div>
         <Navbar />
