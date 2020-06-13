08e5bf83c824f066c7aaa651a5b82eb5df7695ba
jguzz <joseph.a.guzzardo@gmail.com>
2020-06-13 16:12:26 -0400

    Collections now show their stories when clicked

--- src/components/collection/Collection.js
+++ src/components/collection/Collection.js
@@ -1,14 +1,16 @@
 import React from "react";
 // Material UI
 import Card from "@material-ui/core/Card";
+import { Link } from "react-router-dom";
 
-function Collection({ created_at, description, id, name, user_id }) {
+function Collection({ posts,stories,created_at, description, id, name, user_id, }) {
   return (
     <>
-      <Card >
-        <p>Name: {name}</p>
-        <p>Description: {description}</p>
-      </Card>
+
+        <Card component={Link} to={{pathname:`/profile/collection_list/${id}`, state:{name: name, description,stories,posts}}}>
+          <p>Name: {name}</p>
+          <p>Description: {description}</p>
+        </Card>
     </>
   );
 }
