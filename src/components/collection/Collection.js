72bbc071bdb898e7e0f3c82e7be54081ef6c5a2a
jguzz <joseph.a.guzzardo@gmail.com>
2020-06-30 09:40:29 -0400

    Collection list styled

--- src/components/collection/Collection.js
+++ src/components/collection/Collection.js
@@ -1,16 +1,57 @@
 import React from "react";
 // Material UI
+import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import { Link } from "react-router-dom";
+import Grid from "@material-ui/core/Grid";
+import { Typography } from "@material-ui/core";
 
-function Collection({ posts,stories,created_at, description, id, name, user_id, likes }) {
+const useStyles = makeStyles((theme) => ({
+   card: {
+     margin: 'auto',
+    marginTop: '4em',
+    height: '50%',
+    width: '50%',
+    backgroundColor: '#00b359',
+    padding: '2em'
+  },
+  text: {
+    color: 'white'
+  }
+}))
+
+function Collection({
+  posts,
+  stories,
+  created_at,
+  description,
+  id,
+  name,
+  user_id,
+  likes,
+}) {
+  const classes = useStyles()
   return (
     <>
-
-        <Card component={Link} to={{pathname:`/profile/collection_list/${id}`, state:{name: name, description,stories,posts}}}>
-          <p>Name: {name}</p>
-          <p>Description: {description}</p>
+  
+      <Grid item xs={12}>
+        <Card
+        className={classes.card}
+          
+          >
+            <Link
+            style={{ textDecoration: 'none' }}
+            to={{
+              pathname: `/profile/collection_list/${id}`,
+              state: { name: name, description, stories, posts }
+            }}>
+            
+          <Typography className={classes.text} variant="h4">{name}</Typography>
+          <Typography className={classes.text} variant="subtitle1">{description}</Typography>
+            </Link>
         </Card>
+      </Grid>
+     
     </>
   );
 }
--- src/components/collection/CollectionList.js
+++ src/components/collection/CollectionList.js
@@ -1,19 +1,74 @@
-import React from 'react';
-import CollectionShow from './CollectionShow'
+import React from "react";
+import CollectionShow from "./CollectionShow";
 import { Route, Switch, Link } from "react-router-dom";
-import Collection from './Collection'
+import Collection from "./Collection";
 
+// Material UI
+import Grid from "@material-ui/core/Grid";
+import { Typography } from "@material-ui/core";
 
-function CollectionList({currentUser, handleChange, comments, commentPostId, comment, deleteComment, handleCommentSubmit,collections, stories,posts, likes, handleLike}) {
+function CollectionList({
+  currentUser,
+  handleChange,
+  comments,
+  commentPostId,
+  comment,
+  deleteComment,
+  handleCommentSubmit,
+  collections,
+  stories,
+  posts,
+  likes,
+  handleLike,
+}) {
   return (
     <>
-    <h3>All Collections</h3>
-    
-    <Switch>
-      <Route exact path={`/profile/collection_list/:id`} render={(collection) => <CollectionShow currentUser={currentUser} handleChange={handleChange} comments={comments} commentPostId={commentPostId} comment={comment} deleteComment={deleteComment} handleCommentSubmit={handleCommentSubmit}  likes={likes}  handleLike={handleLike} stories={stories}posts={posts} {...collection} />}/>
-      <Route exact path="/profile/collection_list" render={() => collections.map(collection => <Collection likes={likes} key={collection.id} {...collection} posts={posts} stories={stories}/> )}/>
-    </Switch>
+      <Typography variant="h4" style={{ textAlign: 'center', color: '#00b359'}} >Collections</Typography>
 
+      <Switch>
+        <Route
+          exact
+          path={`/profile/collection_list/:id`}
+          render={(collection) => (
+            <CollectionShow
+              currentUser={currentUser}
+              handleChange={handleChange}
+              comments={comments}
+              commentPostId={commentPostId}
+              comment={comment}
+              deleteComment={deleteComment}
+              handleCommentSubmit={handleCommentSubmit}
+              likes={likes}
+              handleLike={handleLike}
+              stories={stories}
+              posts={posts}
+              {...collection}
+            />
+          )}
+        />
+        <Route
+          exact
+          path="/profile/collection_list"
+          render={() => (
+            <Grid
+              container
+              direction="row"
+              justify="space-evenly"
+              alignItems="baseline"
+            >
+              {collections.map((collection) => (
+                <Collection
+                  likes={likes}
+                  key={collection.id}
+                  {...collection}
+                  posts={posts}
+                  stories={stories}
+                />
+              ))}
+            </Grid>
+          )}
+        />
+      </Switch>
     </>
   );
 }
